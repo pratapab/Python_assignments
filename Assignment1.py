8e24087db30a10af852596bed65a6af92069f38b
[prata reddy] <anithareddy4196@gmail.com>
2025-10-28 18:45:22 +0530

    firstassignment

---
+++ Assignment1.py
@@ -0,0 +1,13 @@
+# Print even number between 1 ans 20
+
+for number in range (1, 20):
+    if number % 2 == 0:
+        continue
+    else:
+
+         print(number)
+
+# Print numbers from 10 down to 1
+
+for number in range (10, 0, -1):
+    print (number)
